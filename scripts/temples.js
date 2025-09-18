727e831d3ab46322513411f4d62465ab70c637c3
grandeurpawin <gpawin@byupathwat.edu>
2025-09-18 12:32:52 +0800

    Temple Activity

--- scripts/temples.js
+++ scripts/temples.js
@@ -3,7 +3,7 @@ const lastModified = document.querySelector("#lastModified");
 
 const mainNav = document.querySelector('.navigation');
 const hamButton = document.querySelector('#menuButton');
-const headingTwo = document.querySelector('h2')
+const headingTwo = document.querySelector('h1')
 
 currentYear.innerHTML = new Date().getFullYear();
 lastModified.innerHTML = `Last Modification: ${document.lastModified}`;
