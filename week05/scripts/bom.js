b534d3587990abb5dec6de33d84100f8d2d1a2a9
grandeurpawin <gpawin@byupathwat.edu>
2025-12-03 15:05:33 +0800

    Product Form Review

---
+++ week05/scripts/bom.js
@@ -0,0 +1,62 @@
+const input = document.querySelector('#favchap');
+const button = document.querySelector('button');
+const list = document.querySelector('#list');
+
+let chaptersArray = getChapterList() || [];
+
+chaptersArray.forEach(chapter => {
+    displayList(chapter);
+});
+
+button.addEventListener('click', () => {
+    if (input.value != '') {
+        displayList(input.value);
+        chaptersArray.push(input.value);
+        setChaptersList();
+        input.value = '';
+        input.focus();
+}
+});
+
+function displayList(item) {
+    let li = document.createElement('li');
+    let deleteButton = document.createElement('button');
+    li.textContent = item;
+    deleteButton.textContent = '❌';
+    deleteButton.classList.add('delete');
+    li.append(deleteButton);
+    list.append(li);
+
+    deleteButton.addEventListener('click', function () {
+        list.removeChild(li);
+        deleteChapter(li.textContent);
+        input.focus();
+});
+console.log('Adding', item);
+}
+
+function setChaptersList() {
+    localStorage.setItem('myFavBOMList', JSON.stringify(chaptersArray));
+}
+
+function getChapterList() {
+    return JSON.parse(localStorage.getItem('myFavBOMList'));
+}
+
+function deleteChapter(chapter) {
+    chapter = chapter.slice(0, chapter.length - 1);
+    chaptersArray = chaptersArray.filter(item => item !== chapter);
+    setChaptersList();
+}
+
+// Get the current visit count from localStorage, or start at 0
+let visitCount = Number(localStorage.getItem('visitCount')) || 0;
+
+// Increase the count by 1 each time the page loads
+visitCount++;
+
+// Save it back to localStorage
+localStorage.setItem('visitCount', visitCount);
+
+// Show it in the console (or on the page)
+console.log(`You have visited this page ${visitCount} times.`);
